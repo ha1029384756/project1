355e22c92327756d6a71af3f225a959742d1596d
CAU <cau@cau-05.local>
2019-02-08 10:29:44 +0900

    Initial Commit

---
+++ project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by CAU on 08/02/2019.
+//  Copyright © 2019 4561. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
